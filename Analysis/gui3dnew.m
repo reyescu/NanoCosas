adbeaadf5146021e0966e15487b1c3f0a77092ff
reyes <mreyescalvo@gmail.com>
2016-07-02 00:23:49 +0200

    linocuts added some bugs: cursor tag is wrong in axes2. add an option to choose which file to load

--- Analysis/gui3dnew.m
+++ Analysis/gui3dnew.m
@@ -44,54 +44,56 @@ end
 % End initialization code - DO NOT EDIT
 
 
-% --- Executes just before gui3dnew is made visible.
+%% --- Executes just before gui3dnew is made visible.
 function gui3dnew_OpeningFcn(hObject, eventdata, handles, varargin)
 % This function has no output args, see OutputFcn.
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 % varargin   command line arguments to gui3dnew (see VARARGIN)
-%global da ejes
+
 %Load data: change to text field plus load button, then move all the
 %initialization to load button callback
 %global Header Data Ejes
 fi=load('14BR_C.mat');
-global Header Data Ejes Current
-   Header=fi.Header;
-   Data=fi.data;
-   Ejes=fi.ejes;
+%global Current
+   handles.Header=fi.Header;
+   handles.Data=fi.data;
+   handles.Ejes=fi.ejes;
     
 %Initialize slider and z edits
 
 set(handles.zarray_slider,'Min',1);
-set(handles.zarray_slider,'Max',length(Ejes.z_array));
+set(handles.zarray_slider,'Max',length(handles.Ejes.z_array));
 set(handles.zarray_slider,'Value',1);
 
-Current.zmax=Ejes.z_array(end);
-Current.zmin=Ejes.z_array(1);
-Current.zn = get(handles.zarray_slider, 'Value');
-Current.zv =Ejes.z_array(Current.zn_now);
-set(handles.ed_zn, 'String',Current.zn_now);
-set(handles.ed_zv, 'String', Current.zv_now);
+handles.zmax=handles.Ejes.z_array(end);
+handles.zmin=handles.Ejes.z_array(1);
+handles.zn = get(handles.zarray_slider, 'Value');
+handles.zv =handles.Ejes.z_array(handles.zn);
+set(handles.ed_zn, 'String',handles.zn);
+set(handles.ed_zv, 'String', handles.zv);
 
 % plot image
 updateGraph(handles);
 set(handles.axes1,'YDir','normal');
 set(handles.axes1,'Fontsize',14);
-xlabel(handles.axes1,[Ejes.xlabel,'(',Ejes.xunit,')'],'Fontsize',16);
-ylabel(handles.axes1,[Ejes.ylabel,'(',Ejes.yunit,')'],'Fontsize',16);
+xlabel(handles.axes1,[handles.Ejes.xlabel,'(',handles.Ejes.xunit,')'],'Fontsize',16);
+ylabel(handles.axes1,[handles.Ejes.ylabel,'(',handles.Ejes.yunit,')'],'Fontsize',16);
 
 %plot spec
-%Current.pos=[1,1];
-%UpdateGraph2(handles);
-%set(handles.axes2,'YDir','normal');
+handles.pos=[1,1];
+UpdateGraph2(handles);
 set(handles.axes2,'Fontsize',14);
-xlabel(handles.axes2,[Ejes.zlabel,'(',Ejes.zunit,')'],'Fontsize',16);
-ylabel(handles.axes2,[Ejes.datalabel,'(',Ejes.dataunit,')'],'Fontsize',16);
+xlabel(handles.axes2,[handles.Ejes.zlabel,'(',handles.Ejes.zunit,')'],'Fontsize',16);
+ylabel(handles.axes2,[handles.Ejes.datalabel,'(',handles.Ejes.dataunit,')'],'Fontsize',16);
 
+%initialize variables for linecut
+handles.firstclick=1;
+handles.pos2=[1,1];
 
+% Choose default command line output for gui3dnew
 handles.output = hObject;
-
 
 %setup cursor
 dcm_obj = datacursormode(gcf);
@@ -99,11 +101,6 @@ dcm_obj = datacursormode(gcf);
   set(dcm_obj,'UpdateFcn', @myupdatefcn )
   handles.dcm_obj = datacursormode(hObject);
  set(handles.dcm_obj,'Enable','on','UpdateFcn',{@myupdatefcn,hObject});
-
-
-
-
-% Choose default command line output for gui3dnew
 
 % Update handles structure
 guidata(hObject, handles);
@@ -130,12 +127,11 @@ varargout{1} = handles.output;
 function zarray_slider_Callback(hObject, eventdata, handles)
 % hObject    handle to zarray_slider (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-global Ejes Current   
-Current.zn = round(get(hObject, 'Value'));
-set(handles.ed_zn,'String',Current.zn);
-   Current.zv = Ejes.z_array(Current.zn);
-   set(handles.ed_zv, 'String', Current.zv); 
+% handles    structure with handles and user data (see GUIDATA)   
+handles.zn = round(get(hObject, 'Value'));
+set(handles.ed_zn,'String',handles.zn);
+   handles.zv = handles.Ejes.z_array(handles.zn);
+   set(handles.ed_zv, 'String', handles.zv); 
    updateGraph(handles);
 
 
@@ -169,22 +165,22 @@ function ed_zv_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of ed_zv as text
 %        str2double(get(hObject,'String')) returns contents of ed_zv as a double
-global Ejes Current   
-Current.zv = str2double(get(hObject, 'String'));
-        if (Current.zv > Current.zmax) 
-            Current.zn=length(Ejes.z_array);
-            Current.zv=Current.zmax;
-        else if (Current.zv < Current.zmin)
-                Current.zn=1;
-                Current.zv=Current.zmin;
+   
+handles.zv = str2double(get(hObject, 'String'));
+        if (handles.zv > handles.zmax) 
+            handles.zn=length(handles.Ejes.z_array);
+            handles.zv=handles.zmax;
+        else if (handles.zv < handles.zmin)
+                handles.zn=1;
+                handles.zv=handles.zmin;
             else
-            Current.zn = find(Ejes.z_array > Current.zv,1);
+            handles.zn = find(handles.Ejes.z_array > handles.zv,1);
             end
         end
-        set(handles.zarray_slider,'value', Current.zn);
-        set(handles.ed_zn,'String',Current.zn);
-        Current.zv = Ejes.z_array(Current.zn);
-        set(handles.ed_zv, 'String', Current.zv); 
+        set(handles.zarray_slider,'value', handles.zn);
+        set(handles.ed_zn,'String',handles.zn);
+        handles.zv = handles.Ejes.z_array(handles.zn);
+        set(handles.ed_zv, 'String', handles.zv); 
         updateGraph(handles)
         
         
@@ -207,9 +203,19 @@ function holdon_bt_Callback(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 
 % Hint: get(hObject,'Value') returns toggle state of holdon_bt
- global Data Ejes Current
-  axes(handles.axes2);
-        plot(Ejes.z_array, squeeze(Data(Current.pos(1),Current.pos(2),:)));
+ if hObject.Value== 0
+     hObject.String='Hold Off';
+     hold(handles.axes1,'off');
+     hold(handles.axes2,'off');
+     updateGraph(handles);
+ else
+     hObject.String='Hold on';
+     
+ end
+ if get(handles.linecut_bt,'Value')==1
+     set(handles.linecut_bt,'Value',0);
+              updateGraph(handles);
+ end
 
 
 
@@ -220,16 +226,22 @@ function linecut_bt_Callback(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 
 % Hint: get(hObject,'Value') returns toggle state of linecut_bt
+         updateGraph(handles);
+
+if get(handles.holdon_bt,'Value')==1
+    set(handles.holdon_bt,'Value',0);
+end
+
 
 function updateGraph(handles)
-global Data Ejes Current
-        test=squeeze(Data(:,:,Current.zn));
+%global  Current
+        test=squeeze(handles.Data(:,:,handles.zn));
         imagesc(test','Parent',handles.axes1);
          colorbar(handles.axes1);
         set(handles.axes1,'YDir','normal');
         set(handles.axes1,'Fontsize',14);
-        xlabel(handles.axes1,[Ejes.xlabel,'(',Ejes.xunit,')'],'Fontsize',16);
-        ylabel(handles.axes1,[Ejes.ylabel,'(',Ejes.yunit,')'],'Fontsize',16);
+        xlabel(handles.axes1,[handles.Ejes.xlabel,'(',handles.Ejes.xunit,')'],'Fontsize',16);
+        ylabel(handles.axes1,[handles.Ejes.ylabel,'(',handles.Ejes.yunit,')'],'Fontsize',16);
        
         % Update frequency and amplitude text
        
@@ -244,11 +256,11 @@ function ed_zn_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of ed_zn as text
 %        str2double(get(hObject,'String')) returns contents of ed_zn as a double
-global Ejes Current   
-Current.zn = round(str2double(get(hObject, 'String')));
-set(handles.zarray_slider,'Value',Current.zn);
-   Current.zv = Ejes.z_array(Current.zn);
-   set(handles.ed_zv, 'String', Current.zv); 
+%global  Current   
+handles.zn = round(str2double(get(hObject, 'String')));
+set(handles.zarray_slider,'Value',handles.zn);
+   handles.zv = handles.Ejes.z_array(handles.zn);
+   set(handles.ed_zv, 'String', handles.zv); 
    updateGraph(handles);
 
 
@@ -266,29 +278,46 @@ end
 
 
 function txt = myupdatefcn(~, event_obj,hFigure)
-  global Ejes Data Current
+  %global  Current
   %axes(handles.axes1);
    hAxesParent  = get(get(event_obj,'Target'),'Parent');
-   pos = get(event_obj,'Position');
-   hand=guidata(hFigure);
-   if hAxesParent==hand.axes1
+   position = get(event_obj,'Position');
+   handles=guidata(hFigure);
+   if hAxesParent==handles.axes1
    %disp('hey') 
-   Current.pos = pos;
-   %plot(hand.axes2,Ejes.z_array, squeeze(Data(Current.pos(1),Current.pos(2),:)));
-   UpdateGraph2(hand)
-   txt = [num2str(pos(1)),',',num2str(pos(2)),',',num2str(Data(pos(1),pos(2),Current.zn))];
+     if get(handles.linecut_bt,'Value') == 1
+         updateGraph(handles);
+      if handles.firstclick==1
+          handles.pos = position;
+          handles.firstclick=0;
+          %disp('hey')
+      else
+          handles.pos2 = position;
+          plottheline(handles);
+          handles.firstclick=1;
+        %  guidata(hFigure, handles);
+         % disp('ho')
+      end 
+
+     else
+   handles.pos = position;
+   %plot(hand.axes2,handles.Ejes.z_array, squeeze(handles.Data(Current.pos(1),Current.pos(2),:)));
+     UpdateGraph2(handles)
+     end
+   txt = [num2str(handles.pos(1)),',',num2str(handles.pos(2)),',',num2str(handles.Data(handles.pos(1),handles.pos(2),handles.zn))];
    %disp(['You clicked X:',num2str(Current.pos(1)),', Y:',num2str(Current.pos(2)),',Z:',num2str(Current.zn)]);
 
    else
-      txt = [num2str(pos(1)),',',num2str(pos(2))];
+      txt = [num2str(handles.pos(1)),',',num2str(handles.pos(2))];
    %disp(['You clicked X:',num2str(Current.pos(1)),', Y:',num2str(Current.pos(2))]);
    end
-   if get(hand.holdon_bt,'Value') == 1
-   hold(hand.axes1,'on')
-   hold(hand.axes2,'on')
-   scatter(hand.axes1,pos(1),pos(2),100,'filled')
+   if get(handles.holdon_bt,'Value') == 1
+   hold(handles.axes1,'on')
+   hold(handles.axes2,'on')
+   scatter(handles.axes1,handles.pos(1),handles.pos(2),100,'filled')
    end
-   
+         guidata(hFigure, handles);
+
    %if 
    %        scatter(hand.axes1,pos(1),pos(2),'filled');
    %     end
@@ -310,8 +339,43 @@ function txt = myupdatefcn(~, event_obj,hFigure)
  
  
     function UpdateGraph2(handles)
-        global Data Ejes Current
-        plot(handles.axes2,Ejes.z_array, squeeze(Data(Current.pos(1),Current.pos(2),:)));
+        %global  Current
+        plot(handles.axes2,handles.Ejes.z_array, squeeze(handles.Data(handles.pos(1),handles.pos(2),:)));
         set(handles.axes2,'Fontsize',14);
-        xlabel(handles.axes2,[Ejes.zlabel,'(',Ejes.zunit,')'],'Fontsize',16);
-        ylabel(handles.axes2,[Ejes.datalabel,'(',Ejes.dataunit,')'],'Fontsize',16);
+        xlabel(handles.axes2,[handles.Ejes.zlabel,'(',handles.Ejes.zunit,')'],'Fontsize',16);
+        ylabel(handles.axes2,[handles.Ejes.datalabel,'(',handles.Ejes.dataunit,')'],'Fontsize',16);
+
+        
+  function plottheline(handles)
+     % global   Current
+      x1=handles.pos(1);
+      x2=handles.pos2(1);
+      y1=handles.pos(2);
+      y2=handles.pos2(2);
+      if abs(x2-x1)>abs(y2-y1)
+       mm=(y2-y1)./(x2-x1);
+                    if x1>x2; 
+                        xprof=x2:x1;
+                    else
+                        xprof=x1:x2;
+                    end
+                    
+                    yprof=y1+round(mm.*(xprof-xprof(1)));
+                 else
+                     mm=(x2-x1)./(y2-y1);
+                    if y1>y2; yprof=y2:y1 ;
+                    else yprof=y1:y2; end
+                    xprof=x1+round(mm.*(yprof-yprof(1)));
+      end
+                 hold(handles.axes1,'on')
+                 plot(handles.axes1,xprof,yprof,'black','Linewidth',2);
+                 for i=1:length(xprof);
+                 handles.eeeh(i,:)=squeeze(handles.Data(xprof(i),yprof(i),:));
+                 end
+                 %eeeh(1,1);
+                 %size(eeeh);
+                 %hold(handles.axes2,'off');
+                 h=figure();
+                 imagesc(handles.eeeh');
+                 %Current.firstclick=true;
+    
